d5db2529105607a22e5484cee559b1fb80052f3c
Matt Miller <mattmillerf@pm.me>
2023-02-21 10:22:18 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs, io::Write};
 use clap::Parser;
 use anyhow::Result;
-use random_string::generate;
+use rand::{distributions::Alphanumeric, Rng};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -19,7 +19,6 @@ fn main() -> Result<()>{
     let args = Args::parse();
     let size = args.lines;
     let path = args.path; 
-    let charset = "abcdefghijklmnopqrstuvwxyz";
 
     let mut file = fs::OpenOptions::new()
         .write(true)
@@ -28,9 +27,30 @@ fn main() -> Result<()>{
         .open(path)?;
 
     for _ in 0..size {
-        let string = generate(8, charset);
+        let string = generate_string(8);
         writeln!(file, "{}", string)?;
     }
 
     Ok(())
+}
+
+
+/// Generate a random string of Alphanumeric characters.
+///
+/// # Arguments
+///
+/// * `length` - The length of the string. 
+///
+/// # Examples
+///
+/// ```
+/// generate_string(8) // "fasdhlkk"
+/// ```
+fn generate_string(length: usize) -> String {
+    let s: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(length)
+        .map(char::from)
+        .collect();
+    return s;
 }
